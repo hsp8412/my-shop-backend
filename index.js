0e5c59175c9524be1def7111ba156cfd323fab49
Sipeng He <hsp8412@hotmail.com>
2022-10-16 22:04:06 -0600

    Delete sync from index.js

--- index.js
+++ index.js
@@ -9,47 +9,50 @@ const orderRoutes = require("./routes/order");
 const cartRoutes = require("./routes/cart");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 server.use(cors());
 
-server.post('/webhook', bodyParser.raw({type: 'application/json'}), async (request, response) => {
-  const payload = request.body;
-  const sig = request.headers['stripe-signature'];
-  const endpointSecret = process.env.END_POINT_SECRET;
+server.post(
+  "/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  async (request, response) => {
+    const payload = request.body;
+    const sig = request.headers["stripe-signature"];
+    const endpointSecret = process.env.END_POINT_SECRET;
 
-  let event;
+    let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-  } catch (err) {
-    return response.status(400).send(`Webhook Error: ${err.message}`);
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (err) {
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    const fulfillOrder = async (session) => {
+      //get the order
+      const order = await db.order.findOne({
+        where: { uuid: session.metadata.orderId },
+      });
+      //change status
+      order.set({
+        status: "confirmed",
+      });
+      await order.save();
+      console.log("Fulfilling order", session);
+    };
+
+    // Handle the checkout.session.completed event
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      // Fulfill the purchase...
+      await fulfillOrder(session);
+    }
+
+    response.status(200).end();
   }
-
-
-  const fulfillOrder = async (session) => {
-    //get the order
-    const order = await db.order.findOne({
-      where: {uuid: session.metadata.orderId}
-    });
-    //change status
-    order.set({
-      status: "confirmed"
-    })
-    await order.save();
-    console.log("Fulfilling order", session);
-  }
-
-  // Handle the checkout.session.completed event
-  if (event.type === 'checkout.session.completed') {
-    const session = event.data.object;
-
-    // Fulfill the purchase...
-    await fulfillOrder(session);
-  }
-
-  response.status(200).end();
-});
+);
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
@@ -62,8 +65,8 @@ server.use("/api/cart", cartRoutes);
 
 const port = process.env.PORT || 4000;
 
-db.sequelize.sync({ alter: true }).then(() => {
-  server.listen(port, () => {
-    console.log(`listening at: http://localhost:${port}`);
-  });
+// db.sequelize.sync({ alter: true }).then(() => {
+server.listen(port, () => {
+  console.log(`listening at: http://localhost:${port}`);
 });
+// });
